d61c203e9ba9e345be34ee8aaadf8e006ba3cd79
Scouser87 <scouser987@gmail.com>
2016-03-13 13:07:11 +0300

    updated ShotviewController

--- ShotsTest/ShotsViewController.swift
+++ ShotsTest/ShotsViewController.swift
@@ -18,6 +18,12 @@ class ShotsViewController: UITableViewController {
     
     var api : DribbbleAPI!
     
+    var shotsIsUpdating : Bool?
+    
+    var container: UIView = UIView()
+    var loadingView: UIView = UIView()
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -28,6 +34,7 @@ class ShotsViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
         self.api = DribbbleAPI()
+        self.shotsIsUpdating = false
         
         api.loadLocalShots(didLoadShots)
         updateShots()
@@ -41,10 +48,14 @@ class ShotsViewController: UITableViewController {
             self.shots.append(shot)
         }
         self.tableView.reloadData()
+        self.shotsIsUpdating = false
+        hideActivityIndicator(self.view)
     }
     
     func updateShots(){
         
+        self.shotsIsUpdating = true
+        showActivityIndicator(self.view)
         let url = "https://api.dribbble.com/v1/shots"
         api.loadShots(url, completion: didLoadShots)
     }
@@ -138,5 +149,48 @@ class ShotsViewController: UITableViewController {
         }        
     }
     
+    override func scrollViewDidScroll(scrollView: UIScrollView!) {
+        let currentOffset = scrollView.contentOffset.y
+        let maximumOffset = scrollView.contentSize.height - scrollView.frame.size.height
+        let deltaOffset = maximumOffset - currentOffset
+        
+        if deltaOffset <= 0 {
+            if self.shotsIsUpdating != true {
+                updateShots()
+            }
+        }
+    }
+    
+    func showActivityIndicator(uiView: UIView) {
+        container.frame = uiView.frame
+        container.center = uiView.center
+        container.backgroundColor = UIColorFromHex(0xffffff, alpha: 0.3)
+        
+        loadingView.frame = CGRectMake(0, 0, 80, 80)
+        loadingView.center = uiView.center
+        loadingView.backgroundColor = UIColorFromHex(0x444444, alpha: 0.7)
+        loadingView.clipsToBounds = true
+        loadingView.layer.cornerRadius = 10
+        
+        activityIndicator.frame = CGRectMake(0.0, 0.0, 40.0, 40.0);
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.WhiteLarge
+        activityIndicator.center = CGPointMake(loadingView.frame.size.width / 2, loadingView.frame.size.height / 2);
+        
+        loadingView.addSubview(activityIndicator)
+        container.addSubview(loadingView)
+        uiView.addSubview(container)
+        activityIndicator.startAnimating()
+    }
 
+    func hideActivityIndicator(uiView: UIView) {
+        activityIndicator.stopAnimating()
+        container.removeFromSuperview()
+    }
+    
+    func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+    }
 }
